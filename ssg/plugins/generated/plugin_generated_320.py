333f948c0ab17ca6c29f73ce6e1b937bc8bb99f9
a-micable <a-micable@users.noreply.github.com>
2015-11-16 12:00:00 +0300

    chore(seed): add generated post 320

---
+++ ssg/plugins/generated/plugin_generated_320.py
@@ -0,0 +1,15 @@
+"""
+Auto-generated plugin 320
+"""
+from typing import Dict
+
+def setup(context: Dict):
+    """Plugin setup for generated plugin 320"""
+    # harmless transform
+    def on_page_render(page):
+        return page
+    context.setdefault('hooks', []).append(on_page_render)
+    return True
+
+def teardown():
+    return None
