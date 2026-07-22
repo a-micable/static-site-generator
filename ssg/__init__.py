2d7ad90930accb9d83b5a98bbbf15ce2f18a64a3
a-micable <a-micable@users.noreply.github.com>
2026-07-22 12:28:12 +0300

    chore(rewrite): tweak ssg/__init__.py (part 205)

---
+++ ssg/__init__.py
@@ -0,0 +1,27 @@
+"""Static Site Generator - production-grade Python SSG."""
+
+from ssg.config import SiteConfig
+from ssg.content import ContentIndex, ContentQuery
+from ssg.exceptions import (
+    BuildError,
+    ConfigError,
+    ParseError,
+    PluginError,
+    RenderError,
+    SSGError,
+)
+
+__version__ = "1.0.0"
+__all__ = [
+    "BuildError",
+    "ConfigError",
+    "ContentIndex",
+    "ContentQuery",
+    "ParseError",
+    "PluginError",
+    "RenderError",
+    "SSGError",
+    "SiteConfig",
+    "__version__",
+]
+# rewrite commit 205
